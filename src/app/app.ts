012637f7c6e6440762aef8141bfea576f67f74ae
michael-small <michaelsmalldev@gmail.com>
2025-08-29 02:08:51 +0000

    delete pre-main stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,7 @@
 import { Component, linkedSignal, Signal, signal } from '@angular/core';
-import { LinkedSignal } from './examples/linked-signal';
-import { Resource } from './examples/resource';
-import { ReactiveFormComponent } from '../reactive-form.service';
-import { SignalFormComponent } from '../signal-form.service';
-import { Parent } from './child-form-example/parent';
 
 @Component({
   selector: 'app-root',
-  template: ` <app-parent /> `,
-  imports: [Parent],
+  template: ``,
 })
 export class App {}

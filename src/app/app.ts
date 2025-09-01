95ed7a7c4c7b158afd19444060a6bc8bec8e96fb
michael-small <michaelsmalldev@gmail.com>
2025-09-01 17:56:35 +0000

    feat: add cancel service form example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,61 @@
-import { Component, linkedSignal, Signal, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Control, form, required } from '@angular/forms/signals';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+    <h2>Cancel Service Form</h2>
+
+    <form id="cancel-service-form">
+      <mat-button-toggle-group
+        name="cancelReason"
+        aria-label="Reason to Cancel"
+        [control]="cancelServiceForm.reason"
+      >
+        <mat-button-toggle value="user">Cost</mat-button-toggle>
+        <mat-button-toggle value="spam">No longer use</mat-button-toggle>
+        <mat-button-toggle value="other">Other</mat-button-toggle>
+      </mat-button-toggle-group>
+
+      @if(cancelServiceForm.reason().value() === 'other') {
+      <mat-form-field>
+        <mat-label>Other Explanation</mat-label>
+        <textarea
+          matInput
+          [control]="cancelServiceForm.otherExplanation"
+        ></textarea>
+      </mat-form-field>
+      }
+
+      <button mat-raised-button [disabled]="!cancelServiceForm().valid()">
+        Submit
+      </button>
+    </form>
+  `,
+  imports: [
+    Control,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+  ],
+  styleUrls: [`./styles.scss`],
 })
-export class App {}
+export class App {
+  readonly cancelServiceForm = form(
+    signal({ reason: '', otherExplanation: '' }),
+    (path) => {
+      required(path.reason);
+      required(path.otherExplanation, {
+        when({ valueOf }) {
+          return valueOf(path.reason) === 'other';
+        },
+      });
+    }
+  );
+}
